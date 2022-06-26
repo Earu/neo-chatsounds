ef2faa57918ec12e04695f848bd7370eccbfefc2
Earu <earu.derg@gmail.com>
2022-06-26 23:10:02 +0200

    this needs to be a number

--- lua/neo-chatsounds/modifiers/loop.lua
+++ lua/neo-chatsounds/modifiers/loop.lua
@@ -5,13 +5,13 @@ MODIFIER.DefaultValue = 0
 
 function MODIFIER:ParseArgs(args)
 	local n = tonumber(args)
-	if not n then return 0 end
+	if not n then return self.DefaultValue end
 
 	return math.max(0, n)
 end
 
 function MODIFIER:OnStreamInit(stream)
-	stream:SetLooping(self:GetValue() ~= '0')
+	stream:SetLooping(self:GetValue() ~= 0)
 end
 
 return MODIFIIER
