966de430c7918a98976970e27c0d7fd7203e1e4b
Earu <earu.derg@gmail.com>
2022-06-29 20:00:18 +0200

    move select modifier completion to the modifier file

--- lua/neo-chatsounds/modifiers/select.lua
+++ lua/neo-chatsounds/modifiers/select.lua
@@ -16,4 +16,34 @@ function MODIFIER:OnSelection(index, matching_sounds)
 	return self.Value, matching_sounds
 end
 
+local INDEX_SELECTION_PATTERN = "#(%d+)$"
+local INDEX_SELECTION_NO_ARGS_PATTERN = "#$"
+function MODIFIER.OnCompletion(text, suggestions, added_suggestions)
+	local match = text:match(INDEX_SELECTION_PATTERN) or text:match(INDEX_SELECTION_NO_ARGS_PATTERN)
+	if not match then return false end
+
+	local index = tonumber(match) or 1
+
+	text = text:gsub(INDEX_SELECTION_PATTERN, ""):gsub(INDEX_SELECTION_NO_ARGS_PATTERN, "")
+	local sounds = chatsounds.Parser.ParseSoundTriggers(text)
+	if #sounds == 0 then return false end
+
+	local last_sound = sounds[#sounds]
+	local existing_sounds = chatsounds.Data.Lookup.List[last_sound.Key]
+
+	index = math.min(index, #existing_sounds)
+
+	for i = index, #existing_sounds + index do
+		local relative_index = math.max(1, i % (#existing_sounds + 1))
+		local sound_data = existing_sounds[relative_index]
+		if not added_suggestions[sound_data.Url] then
+			local suggestion = ("%s%s#%d%s"):format(string.sub(text, 1, last_sound.StartIndex - 1), last_sound.Key, relative_index, string.sub(text, last_sound.EndIndex + 1))
+			table.insert(suggestions, { Suggestion = suggestion, Extra = (":realm( %s )"):format(sound_data.Realm) })
+			added_suggestions[sound_data.Url] = true
+		end
+	end
+
+	return true
+end
+
 return MODIFIER
