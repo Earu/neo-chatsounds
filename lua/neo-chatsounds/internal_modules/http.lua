145d185b7ca1c12415854f1f3b582a4603e71265
Earu <earu.derg@gmail.com>
2022-07-01 19:22:25 +0200

    maybe fix a few edge cases

--- lua/neo-chatsounds/internal_modules/http.lua
+++ lua/neo-chatsounds/internal_modules/http.lua
@@ -2,15 +2,24 @@ local _http = chatsounds.Module("Http")
 
 function _http.Get(url)
 	local t = chatsounds.Tasks.new()
-	http.Fetch(url, function(body, _, headers, http_code)
-		t:resolve({
-			Body = body,
-			Headers = headers,
-			Status = http_code,
-		})
-	end, function(err)
-		t:reject(err)
-	end)
+	local success = HTTP({
+		method = "GET",
+		url = url,
+		failed = function(err)
+			t:reject(err)
+		end,
+		success = function(http_code, body, headers)
+			t:resolve({
+				Body = body,
+				Headers = headers,
+				Status = http_code,
+			})
+		end,
+	})
+
+	if not success then
+		t:reject("HTTP request failed")
+	end
 
 	return t
 end
